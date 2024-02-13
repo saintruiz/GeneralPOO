7b12589331632c760b118c75d0dd0566b9593254
saintruiz <148170144+saintruiz@users.noreply.github.com>
2024-02-13 23:19:14 +0000

    Ejemplos de polimorfismo agregados

---
+++ practicas/sobrecarga.java
@@ -0,0 +1,23 @@
+class Animal {
+  public void makeSound() {
+    System.out.println("Grr...");
+  }
+}
+class Cat extends Animal {
+  public void makeSound() {
+    System.out.println("Meow");
+  }
+}
+class Dog extends Animal {
+  public void makeSound() {
+    System.out.println("Woof");
+  }
+}
+
+public static void main(String[ ] args) {
+  Animal a = new Dog();
+  Animal b = new Cat();
+}
+
+a.makeSound();
+b.makeSound();
